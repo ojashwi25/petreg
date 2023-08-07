c56c342f4c46024203d08f2cd2812c79a243f03b
ojashwi shahi <ojashwisthakuri@gmail.com>
2023-08-07 14:23:02 +0545

    first commit

---
+++ page.php
@@ -0,0 +1,20 @@
+<?php
+require_once "./connection.php";
+
+    
+   ?> 
+        
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="/style.css">
+</head>
+<body>
+    <p>
+        this is page with cute pictures of pets</p>
+</body>
+</html>
